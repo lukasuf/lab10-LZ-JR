9f0997851ebde4cae664c4f6f1688dcf18838ddc
lukasuf <lukaszhukauskas@ufl.edu>
2025-04-09 14:22:59 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/lukasuf/lab10-LZ-JR/
+# Partner 1: Lukas Zhukauskas
+# Partner 2: Jose Resendiz
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+    return a+b
 
+def subtract(a, b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
+
+def logarithm(a,b):
+    return math.log(b, a)
+
+def exponent(a,b):
+    return a**b
